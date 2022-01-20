2b9113a45f8ddd6c07ebaab74e3cf12b5e010be3
Aparna Bhure <aparnabhure1983@gmail.com>
2022-01-20 14:43:43 +0530

    @Enable Schedule task

--- src/main/java/com/example/DemoRedisRequestCachingNEviction/DemoRedisRequestCachingNEvictionApplication.java
+++ src/main/java/com/example/DemoRedisRequestCachingNEviction/DemoRedisRequestCachingNEvictionApplication.java
@@ -2,8 +2,10 @@ package com.example.DemoRedisRequestCachingNEviction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class DemoRedisRequestCachingNEvictionApplication {
 
 	public static void main(String[] args) {
